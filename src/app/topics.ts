c14667770953bbdeda3d714431af71f498aa4f87
Christoph Buehler <christoph@cbuehler.li>
2018-05-29 20:59:09 +0200

    fixed sub topic resolution

--- src/app/topics.ts
+++ src/app/topics.ts
@@ -1,7 +1,7 @@
 declare const topics: Topic[];
 
 export function subTopics(topic: Topic) {
-    const path = trimSlashes(`${topic.path}/${topic.title.toLocaleLowerCase()}`);
+    const path = trimSlashes(`${topic.path}/${topic.title.toLocaleLowerCase().replace(/ /g, '-')}`);
     const filtered = topics
         .filter(t => t.path.replace(/^\/|\/$/g, '') === path);
     return filtered;
